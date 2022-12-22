857b638c722b35e7ad1e759867de48ac349e5ff8
KhanicaMao <khanica.mao@studentfr.ch>
2022-12-22 08:57:00 +0100

    Rajouter le burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
+var sidenav = document.getElementById("mySidenav");
+var open = document.getElementById("open");
+var close = document.getElementById("close");
 
+open.onclick = openNav;
+close.onclick = closenNav;
+
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+function closenNav() {
+  sidenav.classList.remove("active");
+}
